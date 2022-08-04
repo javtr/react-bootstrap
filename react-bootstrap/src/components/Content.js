fbdb15b57bd9e5d1ae8c7078aa5a04986a4bd66e
javtr <javtriana.z@gmail.com>
2022-08-03 20:50:30 -0500

    consume data

--- react-bootstrap/src/components/Content.js
+++ react-bootstrap/src/components/Content.js
@@ -1,30 +1,34 @@
+import React, { useEffect, useState } from 'react';
 import { Button, Card, Container } from "react-bootstrap";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
-import { useEffect, useState } from 'react';
 
 export default function Content() {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [students, setStudents] = useState([]);
 
-  const[name,setName]=useState('')
-  const[address,setAddress]=useState('')
+  const handleClick = (e) => {
+    e.preventDefault();
+    const student = { name, address };
+    console.log(student);
 
+    fetch("http://localhost:8080/student/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    }).then(() => {
+      console.log("New Student added");
+    });
+  };
 
-  const handleClick=(e)=>{
-    e.preventDefault()
-    const student={name,address}
-    console.log(student)
- 
-    fetch("http://localhost:8080/student/add",{
-      method:"POST",
-      headers:{"Content-Type":"application/json"},
-      body:JSON.stringify(student)
-
-  }).then(()=>{
-    console.log("New Student added")
-  })
-
-
-}
+  useEffect(() => {
+    fetch("http://localhost:8080/student/getAll")
+      .then(res => res.json())
+      .then((result) => {
+        setStudents(result);
+      })
+  }, []);
 
 
   return (
@@ -35,31 +39,43 @@ export default function Content() {
             <Card.Title className="custom-container__tit1">
               Add Student
             </Card.Title>
-
             <FloatingLabel
               controlId="nameInput"
               label="Student Name"
               className="mb-3 custom-container__input"
               value={name}
-              onChange={(e)=>setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
             >
-              <Form.Control  placeholder="Leave a comment here" />
+              <Form.Control placeholder="Leave a comment here" />
             </FloatingLabel>
-
             <FloatingLabel
               controlId="idInput"
               label="Student ID"
               className="mb-3 custom-container__input"
               value={address}
-              onChange={(e)=>setAddress(e.target.value)}
-
+              onChange={(e) => setAddress(e.target.value)}
             >
-              <Form.Control  placeholder="Leave a comment here" />
+              <Form.Control placeholder="Leave a comment here" />
             </FloatingLabel>
+            <Button variant="success" onClick={handleClick}>
+              Submit
+            </Button>{" "}
+          </Card.Body>
+        </Card>
 
+        <Card>
+          <Card.Body>
+            <Card.Title className="custom-container__tit1">Students</Card.Title>
 
-            <Button variant="success" onClick={handleClick}>Submit</Button>{' '}            
-
+            {students.map((student) => (
+              <Card className="custom-container__cardStudent" key={student.id}>
+                Id:{student.id}
+                <br />
+                Name:{student.name}
+                <br />
+                Address:{student.address}
+              </Card>
+            ))}
 
           </Card.Body>
         </Card>
