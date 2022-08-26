a38b6d139c7c6df7e4b520adb1ed7099ed54ebe9
javtr <javtriana.z@gmail.com>
2022-08-25 23:26:32 -0500

    final

---
+++ entregable-frontend-3-junio22/src/components/Item.js
@@ -0,0 +1,34 @@
+// El componente Item no tiene componentes hijos.
+// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
+// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
+// Maqueta de Item:
+//    h3
+//    p
+//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
+//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+
+import React, {useState} from "react"
+
+export default function Item({item,total}) {
+
+  const [stock, setStock] = useState(item.stock)
+
+  function updateStock() {
+    if (stock>0) {
+      setStock(stock-1)
+      total();
+    }
+  }
+
+  return (
+    <div className='producto'>
+      <h3>{item.producto.nombre}</h3>
+      <p>{item.producto.descripcion}</p>
+      <h5>En stock: 
+          {stock>0? stock:<span>agotado</span>}
+      </h5>
+      <button onClick={()=> updateStock()}>{stock>0? 'Comprar':'Agotado'}</button>
+    </div>
+  )
+}
