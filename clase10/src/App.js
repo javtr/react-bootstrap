00a029cb299824f571f0789989d75444da750d31
javtr <javtriana.z@gmail.com>
2022-08-30 22:03:46 -0500

    api

--- clase10/src/App.js
+++ clase10/src/App.js
@@ -1,46 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import Pedido from "./components/pedido";
+import PracticaFetch from "./components/practicaFetch";
 
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      pedido: "",
-      mostrar: true
-    };
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 2000);
-  }
-
-  componentDidUpdate() {
-    console.log('DidUpdate: Justo después de actualizarse');
-}
-
-  tick() {
-    this.setState({ pedido: "pizza" });
-  }
-
-  mostrarOrden = () => {
-    this.setState({mostrar: false});
-  }
-
 
   render() {
-    let myOrder;
-    if (this.state.mostrar) {
-      myOrder = <Pedido pedido = {this.state.pedido}></Pedido>;
-    };
-
-
     return (
       <div className="App">
-        { myOrder}
-        <button onClick={ this.mostrarOrden }>Cancelar pedido</button>
+      <PracticaFetch></PracticaFetch>
       </div>
     );
   }
